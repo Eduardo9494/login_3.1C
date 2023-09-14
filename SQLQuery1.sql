4c1eda4bd5645bff6d80394c3a7d9062dff766c4
Eduardo9494 <1433341@senati.pe>
2023-09-14 10:36:47 -0500

    insertamos roles admin y user y hacemos un selct*from y aparece nuestra tabla

---
+++ SQLQuery1.sql
@@ -0,0 +1,38 @@
+﻿CREATE DATABASE Portalweb
+USE Portalweb
+
+CREATE TABLE Usuarios(
+Cod_Usuario int identity(1,1),
+Nombre varchar(50),
+Apellido varchar(50),
+Correo varchar(70),
+Edad int,
+Usuario varchar(50),
+Contraseña varchar(50)
+)
+
+Alter table Usuarios add Cod_rol int 
+
+select * from Usuarios
+
+Create table Permisos(
+Cod_Permisos int identity(1,1),
+Nombre_Permiso varchar (50)
+)
+
+create table Roles(
+Cod_rol int identity(1,1),
+Nombre_Rol varchar(50)
+)
+
+create table Roles_Permisos(
+Cod_rol_permiso int identity (1,1),
+Cod_Rol int,
+Cod_permiso int,
+Estado bit
+)
+
+insert into Roles values ('Admin')
+insert into Roles values ('User')
+
+select * from Roles
